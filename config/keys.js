d2c3c5836eeffe257c07f14ebe1a7330e9c8d5fb
Ashleigh Barnard <ashleighnbarnard@gmail.com>
2018-07-12 17:17:43 -0400

    validation handlers

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://ashleigh:ashleigh1@ds127771.mlab.com:27771/incrowd'
-}
+  mongoURI: 'mongodb://ashleigh:ashleigh1@ds127771.mlab.com:27771/incrowd',
+  secretOrKey: 'secret'
+};
